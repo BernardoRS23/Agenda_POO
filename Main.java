9e226fa301e6e55f82051e2ad46c3af3d6e8fb48
Bernardo Reis Silveira <99258377+BernardoRS23@users.noreply.github.com>
2025-10-02 12:40:50 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 package agenda;
+import java.util.scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        
+        Agenda agenda = new Agenda();
+        Scanner entrada = new Scanner(System.in);
     }
     
 }
